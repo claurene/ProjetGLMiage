67705bb42d7c22aafb89147b22627cc0a9e8a4d3
Rodolphe Aubry <rodeaubry@gmail.com>
2018-04-16 16:34:35 +0200

    Création de la classe Horaire
    -Création de la classe Horaire (arret, ligne, direction , statut, heure)
    -Nombre de minutes entre deux rames
    -Récuperer la date de prochain passage en fonction de la date actuelle et du point précèdent
    -Création de HoraireTest
        -2 tests

---
+++ src/test/java/miage/model/HoraireTest.java
@@ -0,0 +1,49 @@
+package miage.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Horaire")
+public class HoraireTest {
+
+    @Test
+    @DisplayName("Changement de direction")
+    void changementDirection(){
+        LocalDateTime heureTest = LocalDateTime.now();
+        Station s = new Station("Gare de l'est",2,false,48.79,2.12);
+        ArrayList<Station> listeStation = new ArrayList<Station>();
+        listeStation.add(new Station("Gare du nord",2,false,48.79,2.12));
+        listeStation.add(new Station("Gare de l'est",2,false,48.79,2.12));
+        Ligne l = new Ligne(71,"Metro 5",50,listeStation);
+        Horaire h = new Horaire(s,l,"Nord-Est","En service",heureTest);
+
+        h.setDirection("Est-Nord");
+
+        System.out.println(h.toString());
+        assertEquals(h.getDirection(),"Est-Nord","La direction devrait être Est-Nord");
+    }
+
+
+    @Test
+    @DisplayName("Changement de statut")
+    void changementStatut(){
+        LocalDateTime heureTest = LocalDateTime.now();
+        Station s = new Station("Gare de l'est",2,false,48.79,2.12);
+        ArrayList<Station> listeStation = new ArrayList<Station>();
+        listeStation.add(new Station("Gare du nord",2,false,48.79,2.12));
+        listeStation.add(new Station("Gare de l'est",2,false,48.79,2.12));
+        Ligne l = new Ligne(71,"Metro 5",50,listeStation);
+        Horaire h = new Horaire(s,l,"Nord-Est","En service",heureTest);
+
+        h.setStatut("Annulé");
+
+        assertEquals(h.getStatut(),"Annulé","Le statut devrait être Annulé");
+    }
+}
