83751c704d77c689257e753ad7d8c9b967c4b0ff
claurene <cladt.laurene@gmail.com>
2018-05-04 21:46:11 +0200

    Premiere modelisation des horaires - Calcul du prochain passage d'une rame à la station de départ - Tests sur ces horaires - TODO: direction + temps de parcours entre deux stations

--- src/main/java/miage/model/Horaire.java
+++ src/main/java/miage/model/Horaire.java
@@ -1,7 +1,10 @@
 package miage.model;
 
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 
 /**
  * Classe Horaire qui permet de connaître les horaires de passage des rames sur l'arrêt d'une ligne
@@ -10,6 +13,7 @@ import java.time.LocalDateTime;
 public class Horaire {
 
     private final int NOMBRE_RAMES_HEURE = 10;
+    private final LocalTime DEBUT_RAMES = LocalTime.MIDNIGHT;
     private Station arret;
     private Ligne ligne;
     private String direction;
@@ -33,6 +37,45 @@ public class Horaire {
         this.direction = direction;
         this.statut = statut;
         this.heure = heure;
+    }
+
+    /**
+     * Calcul des horaires de passage des rames à la station de départ de chaque ligne
+     * @return table des horaires
+     */
+    private ArrayList<LocalDateTime> tableHoraireRames(LocalDate date){
+        LocalDateTime heure = LocalDateTime.of(date,DEBUT_RAMES);
+        ArrayList<LocalDateTime> tableHoraire = new ArrayList<LocalDateTime>();
+        for (int i=0;i<24*NOMBRE_RAMES_HEURE;i++){
+            tableHoraire.add(heure);
+            heure = heure.plusMinutes(60/NOMBRE_RAMES_HEURE);
+        }
+        return tableHoraire;
+    }
+
+    private LocalDateTime getDateDepartRame(LocalDateTime heure) {
+        for (LocalDateTime e : tableHoraireRames(heure.toLocalDate())) {
+            if (e.isAfter(heure)) {return e;}
+        }
+        //Si passage après 24h
+        return LocalDateTime.of(heure.toLocalDate().plusDays(1),DEBUT_RAMES);
+    }
+
+    /**
+     * Obtenir l'horaire désiré en fonction de la ligne, la station, la direction et l'heure choisies
+     * @return l'horaire approprié
+     */
+    public LocalDateTime getHoraire() {
+        //TODO : gérer la direction !!
+        Station departLigne = this.ligne.getDepart();
+        LocalDateTime horaire = this.heure; //initialisation
+        // Si la station est un départ de ligne :
+        if (this.arret.getNomStation().equals(departLigne.getNomStation())) {
+            horaire = getDateDepartRame(this.heure);
+        } else {
+            //TODO : avec temps de parcours entre deux stations
+        }
+        return horaire;
     }
 
     /**
@@ -106,7 +149,7 @@ public class Horaire {
 
     @Override
     public String toString() {
-        return "Le prochain passage d'une rame à l'arrêt : " + arret + " de la ligne "+ ligne +" direction "+ direction + " se fera le " + getProchainPassage();
+        return "Le prochain passage d'une rame à l'arrêt : " + arret.getNomStation() + " de la ligne "+ ligne.getNomLigne() +" direction "+ direction + " se fera le " + getProchainPassage();
     }
 
 }
--- src/test/java/miage/model/HoraireTest.java
+++ src/test/java/miage/model/HoraireTest.java
@@ -3,7 +3,9 @@ package miage.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertFalse;
@@ -49,5 +51,62 @@ public class HoraireTest {
         h.setStatut("Annulé");
 
         assertEquals(h.getStatut(),"Annulé","Le statut devrait être Annulé");
+    }
+
+    @Test
+    @DisplayName("Horaire pour une station de départ")
+    void horaireDepart(){
+        LocalDateTime heureTest = LocalDateTime.of(LocalDate.now(), LocalTime.of(6,0));
+        ArrayList<Integer> tempsParcours = new ArrayList<>();
+        tempsParcours.add(10);
+        Station s = new Station("Gare du nord",2,false,48.79,2.12);
+        ArrayList<Station> listeStation = new ArrayList<Station>();
+        listeStation.add(new Station("Gare du nord",2,false,48.79,2.12));
+        listeStation.add(new Station("Gare de l'est",2,false,48.79,2.12));
+        Ligne l = new Ligne("Metro 5",tempsParcours,listeStation);
+
+        Horaire h = new Horaire(s,l,"Nord-Est","En service",heureTest);
+        System.out.println(h.getHoraire());
+
+        assertEquals(h.getHoraire(),LocalDateTime.of(LocalDate.now(),LocalTime.of(6,6)));
+
+    }
+
+    @Test
+    @DisplayName("Horaire pour une station quelconque")
+    void horaireStation(){
+        LocalDateTime heureTest = LocalDateTime.now();
+        ArrayList<Integer> tempsParcours = new ArrayList<>();
+        tempsParcours.add(10);
+        Station s = new Station("Gare de l'est",2,false,48.79,2.12);
+        ArrayList<Station> listeStation = new ArrayList<Station>();
+        listeStation.add(new Station("Gare du nord",2,false,48.79,2.12));
+        listeStation.add(new Station("Gare de l'est",2,false,48.79,2.12));
+        Ligne l = new Ligne("Metro 5",tempsParcours,listeStation);
+
+        Horaire h = new Horaire(s,l,"Nord-Est","En service",heureTest);
+        System.out.println(h.getHoraire());
+
+        assertEquals(h.getHoraire(),heureTest);
+
+    }
+
+    @Test
+    @DisplayName("Horaire pour une station après 24h")
+    void horaire24H(){
+        LocalDateTime heureTest = LocalDateTime.of(LocalDate.now(), LocalTime.of(23,55));
+        ArrayList<Integer> tempsParcours = new ArrayList<>();
+        tempsParcours.add(10);
+        Station s = new Station("Gare du nord",2,false,48.79,2.12);
+        ArrayList<Station> listeStation = new ArrayList<Station>();
+        listeStation.add(new Station("Gare du nord",2,false,48.79,2.12));
+        listeStation.add(new Station("Gare de l'est",2,false,48.79,2.12));
+        Ligne l = new Ligne("Metro 5",tempsParcours,listeStation);
+
+        Horaire h = new Horaire(s,l,"Nord-Est","En service",heureTest);
+        System.out.println(h.getHoraire());
+
+        assertEquals(h.getHoraire(),LocalDateTime.of(heureTest.toLocalDate().plusDays(1),LocalTime.MIDNIGHT));
+
     }
 }
