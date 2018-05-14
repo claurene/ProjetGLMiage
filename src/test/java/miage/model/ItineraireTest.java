fa2226dd57dbfb55b32ff90bb2dbab0f60d4f5f7
Charlotte Fernandes <fernandescharlotte54@gmail.com>
2018-05-14 14:35:42 +0200

    Debug itineraireTest
    le test pour l'itinéraire le plus rapide est fonctionnel

--- src/test/java/miage/model/ItineraireTest.java
+++ src/test/java/miage/model/ItineraireTest.java
@@ -1,6 +1,7 @@
 package miage.model;
 
 import miage.controller.LigneController;
+import miage.controller.StationController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,15 +10,17 @@ import org.junit.jupiter.api.Test;
 @DisplayName("Itinéraire")
 public class ItineraireTest {
     private final LigneController ligneController = new LigneController();
+    private final StationController stationController = new StationController();
 
     @Test
     @DisplayName("Itinéraire rapide avec le jeu de données")
     void itineraireRapide(){
         ligneController.initialisationLignes();
+        stationController.initialisationStations();
 
         Graphe g = new Graphe(ligneController.getLignes());
         Itineraire i = new Itineraire(g,"temple","bastille");
 
-        System.out.println(i.constItineraireRapide().toString());
+        i.constItineraireRapide();
     }
 }
